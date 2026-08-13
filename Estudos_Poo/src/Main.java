81c7f4e2a79e8bb2bbcc558e1b949666d07f9bce
Edson Not <edson.contatos0@gmail.com>
2026-08-13 00:02:28 -0300

    :tada: Commit inicial estudos_poo

---
+++ Estudos_Poo/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int i =0;
+        float f=0;
+        double d= 0.0;
+        boolean b = true;
+        char c = 'c';
+        String texto = "Teste";
+
+
+
+    }
+}
