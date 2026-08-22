81eccd7389bd1acbb9c788ab6c107977f0234c47
Edson Not <edson.contatos0@gmail.com>
2026-08-22 19:15:18 -0300

    :construction: Em desenvolvimento

--- Estudos_Poo/Lista_1/src/Agenda.java
+++ Estudos_Poo/Lista_1/src/Agenda.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class Agenda {
+    ArrayList <Pessoa>caderneta; // Criação do objeto lista
+
+    public Agenda(){
+        caderneta = new ArrayList<>();  // Instanciado o objeto "caderneta" e chamando o o objeto "new Array list<>"
+    }
+
+    public void adicionarPessoa (Pessoa p){
+        caderneta.add(p);
+    }
+
+    public void imprimir(){
+        //FOREACH
+        for(Pessoa p: caderneta){
+            p.imprimirPessoa();
+        }
+      /*
+        for(int i=0; i<caderneta.size(); i++){ // Size - tamanho da lista - vem da class ArrayList
+            caderneta.get(i); //Get : recupera uma posição da lista - vem da class ArrayList
+        }
+      */
+
+    }
 }
+
+
+
+// ArrayList aceita somente tipos abstratos
+//Wrapper Class : Classes de embrulho = int = Interger
+
+/* conversão
+* interger.parseInt(string)
+* Float.parseFloat(string)
+* */
+
+
+//lista = new ArrayList<>(); // Instanciação
--- Estudos_Poo/Lista_1/src/Main.java
+++ Estudos_Poo/Lista_1/src/Main.java
@@ -1,7 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Teste");
+        Scanner input = new Scanner(System.in);
+        Agenda agendaContatos = new Agenda();
+        //Pessoa p = new Pessoa();
+
+    int opcao = 0;
+
+    do{
+        System.out.println("1 - Inserir Pessoa \n" + "2 - Imprimir pessoa \n" + "3 - imprimir agenda \n" + "0 - Sair \n" +"Selecione uma das opcaoes: ");
+        opcao = input.nextInt();
+        switch (opcao){
+            case 1->{
+                Pessoa p = new Pessoa();
+                System.out.println("Digite o nome da pessoa: ");
+                p.nome = input.next();
+
+                int opcao2 = 0; // variavrl para controle do 2 DoWhile;
+                do{
+                    System.out.println("Digite o tipo do contato: ");
+                    String tipo = input.next();
+                    System.out.println("Digite o numero do contatos");
+                    String info = input.next();
+
+                    Contato c = new Contato(tipo, info); // Iniciando o objeto Contato e instanciando, como o objeto possui duas passagem de parametros e necessario cria-los antes de utilizar;
+                    p.adicionarContato(c);
+
+                    System.out.println("Deseja continuar? 1 - Sim 0 - Não");
+                    opcao2 = input.nextInt();
+                }while (opcao2!=0);
+                agendaContatos.adicionarPessoa(p);
+
+            }case 2->{
+             //   p.imprimirPessoa();
+
+            }case 3->{
+                agendaContatos.imprimir();
+
+            }default -> {System.out.println( "Digite uma opcao valida");}
         }
+    }while(opcao !=0);
+
+    }
 }
